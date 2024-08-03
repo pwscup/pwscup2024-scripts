47243611bdd7e204868ab55f81b8f399a9e14f27
kcgunma <158546860+kcgunma@users.noreply.github.com>
2024-08-03 14:51:37 +0900

    Update utilityScoreSingle.py

--- scripts/evaluate/utilityScoreSingle.py
+++ scripts/evaluate/utilityScoreSingle.py
@@ -42,7 +42,7 @@ def find_max_mae_and_columns(file1, file2, parallel=1):
       column_pair = [c1, c2]
       freq1 = df1.value_counts(column_pair)
       freq2 = df2.value_counts(column_pair)
-      err_dic[",".join(column_pair)] = (freq1 - freq2).abs().mean()
+      err_dic[",".join(column_pair)] = (freq1 - freq2).abs().sum()
 
     # MAEの規格化
     max_mae_columns, max_mae = max(err_dic.items(), key=lambda x: x[1])
